4fd4c96081d30529285f1b45f23bb61d2ef3f23f
tunv <nguyenvantu.dhbk@gmail.com>
2015-10-08 22:51:07 +0700

    Add layout and homepage

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,7 +10,5 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index');
+Route::get('home', 'HomeController@index');
---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+@section('title','Home page')
+@section('content')
+    <div class="row">
+        <div class="page-header">
+            <h2>Home Page</h2>
+        </div>
+    </div>
+@endsection
