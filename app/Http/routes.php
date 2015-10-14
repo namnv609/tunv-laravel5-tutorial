25820df24461718be9db68d5a543b2a943517d06
tunv <nguyenvantu.dhbk@gmail.com>
2015-10-14 08:25:50 +0700

    Show all article

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,5 +10,7 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-Route::get('/', 'HomeController@index');
+Route::get('/', 'ArticlesController@index');
 Route::get('home', 'HomeController@index');
+Route::get('articles', 'ArticlesController@index');
+Route::get('article/{id}', 'ArticlesController@show')->where('id', '^[0-9]+$');
